688cd9a7e5caed85a96b5061429f52f8fcc8cdaa
Naugtur <naugtur@gmail.com>
2012-11-25 12:08:44 +0100

    you git...

--- www/js/main.js
+++ www/js/main.js
@@ -1,19 +1,23 @@
+require.config({
+  baseUrl: "/js/",
+});
+
 require(['calendar'], function(Calendar) {
 
   //in case we need it for anything
-  function deviceReady(){}
-  
-  function init(){
-  
+  function deviceReady() {}
+
+  function init() {
+
     //here we init modules
     Calendar.init($('#calendar'));
-  
+
   }
 
 
   document.addEventListener('deviceready', deviceReady, false);
   window.addEventListener('DOMContentLoaded', init, false);
-  
-  
+
+
 
 }
