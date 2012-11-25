772442c7b05f572aaee96b3daeefefc834ed88d5
peterKaleta <peter.kaleta@gmail.com>
2012-11-25 12:35:57 +0100

    s

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,15 +1,30 @@
-define([],function(){
+define([], function() {
 
-  //initializes calendar module, appends its content to given node
-  function init($node){
-    console.log('joł, me running!');
-  
-  }
-  
+	//initializes calendar module, appends its content to given node
 
-  return {
-    init:init
-  
-  }
+	function init($node) {
+
+
+		var auth = function() {
+				var config = {
+					'client_id': '664081262523.apps.googleusercontent.com',
+					'scope': 'https://www.googleapis.com/auth/urlshortener'
+				};
+
+				gapi.auth.authorize(config, function() {
+					console.log('Some motherfucker just logged in');
+					console.log(gapi.auth.getToken());
+				});
+			}
+
+
+
+	}
+
+
+	return {
+		init: init
+
+	}
 
 });
--- www/js/main.js
+++ www/js/main.js
@@ -1,18 +1,20 @@
 require(['calendar'], function(Calendar) {
 
-  //in case we need it for anything
-  function deviceReady() {}
-
-  function init() {
-
-    //here we init modules
-    Calendar.init($('#calendar'));
-
-  }
+	//in case we need it for anything
 
 
-  document.addEventListener('deviceready', deviceReady, false);
-  window.addEventListener('DOMContentLoaded', init, false);
+	function deviceReady() {}
+
+	function init() {
+
+		//here we init modules
+		Calendar.init($('#calendar'));
+
+	}
+
+
+	document.addEventListener('deviceready', deviceReady, false);
+	window.addEventListener('DOMContentLoaded', init, false);
 
 
 
